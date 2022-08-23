ea44b0a86aef473f07c00579e534867925457b96
scalaview <chailink100@gmail.com>
2022-08-23 17:59:25 +0800

    fix screen scroll error, uint 16 are 2 byte

--- src/kernel.c
+++ src/kernel.c
@@ -3,7 +3,7 @@
 void kernel_main(void)
 {
     terminal_screen_initialize();
-    char* str = "hhhhhhhhhhhhhhhhhhhhhhhhh\n";
+    char* str = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh\n";
     int64_t value = 0x123456789ABCD;
     for (int i = 0;i < 25; i++)
         printk("%d, %s", i, str);

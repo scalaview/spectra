7042c3bc727f2858a80b4403c8b3da5617dec96e
scalaview <chailink100@gmail.com>
2022-08-23 23:26:05 +0800

    implement assert

---
+++ src/include/assert.h
@@ -0,0 +1,12 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+#include <stdint.h>
+
+#define assert(e) do {\
+    if(!(e))\
+        assert_error(__FILE__, __LINE__);\
+} while (0)
+
+void assert_error(const char* file, const uint64_t line);
+
+#endif

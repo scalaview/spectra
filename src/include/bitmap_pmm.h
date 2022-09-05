867ba974a8d547e7a096b11c53ce12342225a46f
scalaview <chailink100@gmail.com>
2022-09-05 12:19:56 +0800

    modify all address to virtual address in kernel

--- src/include/bitmap_pmm.h
+++ src/include/bitmap_pmm.h
@@ -1,7 +1,8 @@
 #ifndef BITMAP_PMM_H
 #define BITMAP_PMM_H
+#include "config.h"
 
-#define KERNEL_BITMAP_TABLE_ADDRESS 0x11000
+#define KERNEL_BITMAP_TABLE_ADDRESS (KERNEL_VM_BASE + 0x11000)
 
 extern const struct pmm_manager bitmap_pmm_manager;
 #endif

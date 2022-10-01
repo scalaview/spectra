dee4a91ecda2c87b5d4a7ccebb20df2fb8e01b61
scalaview <chailink100@gmail.com>
2022-10-01 21:46:34 +0800

    remove useless code

--- src/gdt/gdt.asm
+++ src/gdt/gdt.asm
@@ -13,14 +13,3 @@ gdt_load:
     lgdt [rax]
     pop rbp
     ret
-
-
-section .bss
-align 4096
-global ist_1_stack_ptr
-global ist_2_stack_ptr
-user_land_stack:
-    resb RANG3_STACK_SIZE
-ist_1_stack_ptr:
-    resb RANG3_STACK_SIZE
-ist_2_stack_ptr:

2550e2a2a3f4c8c696ce826cc27aacc5fcecea58
scalaview <chailink100@gmail.com>
2022-10-06 12:37:27 +0800

    test multitask

--- programs/apps/start/src/start.c
+++ programs/apps/start/src/start.c
@@ -5,7 +5,7 @@ int main(int argc, char** argv)
     int i = 0;
     while (1)
     {
-        if (i % 100000 == 0)
+        if (i % 1000000 == 0)
             printf("process starts, %d\n", i);
         if (i > 30000000)
             break;

91622c38fa65ae5c970216269520e64d63aaf806
scalaview <chailink100@gmail.com>
2023-02-15 11:44:59 +0800

    implement fio

---
+++ programs/stdlib/src/include/state.h
@@ -0,0 +1,13 @@
+#ifndef OS_STD_STSTE_H
+#define OS_STD_STSTE_H
+
+#define ENOMEM      2
+
+/* FS ERROR */
+#define EBADPATH                100
+#define EIO                     101
+#define EFSNOTUS                102
+#define EFNOTFOUND              103
+
+
+#endif
